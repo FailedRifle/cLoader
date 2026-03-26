47ebcf88b873d677e1c08fb512946bcce53210c8
FailedRifle <139413504+FailedRifle@users.noreply.github.com>
2026-03-26 10:12:05 -0400

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'cheddar-os-v1';
+const ASSETS = [
+  'index.html',
+  'blocked.png'
+];
+
+// Install Service Worker
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Fetching content
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
